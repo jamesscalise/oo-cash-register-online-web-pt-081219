3e0131b23b1260da5b3b342fc6d0601ec624b61d
James Scalise <jamescscalise@gmail.com>
2019-09-02 18:48:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,8 @@ class CashRegister
   def apply_discount
     if discount == 0
       return "There is no discount to apply."
-    else
+    else  
+      @total = @total * ((100-@discount)/100)
       
     
   end
